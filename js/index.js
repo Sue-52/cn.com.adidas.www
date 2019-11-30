89f9d614d5bc98fd7b003339fed3da46c7bdd52e
Sue-52 <1219243947@qq.com>
2019-12-01 00:39:51 +0800

    adidas官网：添加

--- js/index.js
+++ js/index.js
@@ -5,14 +5,22 @@ window.addEventListener('load', function () {
     var carsousel_right = document.querySelecter('.carsousel_right');
     // 要经过的地方，在显示
     var carsousel_first = document.querySelecter('.carsousel_first');
-    twelfth_content.addEventListener('mouseenter', function () {
+    carsousel_first.addEventListener('mouseenter', function () {
         carsousel_left.style.display = 'block';
         carsousel_right.style.display = 'block';
     })
     // 离开轮播图，自动隐藏
-    twelfth_content.addEventListener('mouseleave', function () {
+    carsousel_first.addEventListener('mouseleave', function () {
         carsousel_left.style.display = 'none';
         carsousel_right.style.display = 'none';
     })
-    // 
+    // 生成动态小圆圈，有几张图片，就自动生成几个
+    var ul = carsousel_first.querySelecter('ul');
+    var ol = carsousel_first.querySelecter('circle');
+    // console.log(ul.children.length);
+    for (var i = 0; i < ul.children.length; i++) {
+        var li = document.createElement('li');
+        ol.appendChild(li);
+    }
+    ol.children[0].className = 'current';
 })
