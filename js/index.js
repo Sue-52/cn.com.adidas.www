41f1d4b843b5697579e876fcdd4c279cf92a6fc4
Sue-52 <1219243947@qq.com>
2019-12-01 19:09:09 +0800

    adidas官网：添加

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', function () {
     // alert('1');
     // 1.鼠标经过轮播图，左右按键显示，离开隐藏
-    var carsousel_left = document.querySelecter('.carsousel-left');
+    var carsousel_left = document.querySelecter('.carsousel_left');
     var carsousel_right = document.querySelecter('.carsousel_right');
     // 要经过的地方，在显示
     var carsousel_first = document.querySelecter('.carsousel_first');
